0409966ae74a3d115222b070d1daf1082a637e33
Julian Mercado <julianmercadomeza@gmail.com>
2024-06-17 21:10:56 -0400

    Added finished files from shortly project

---
+++ shortly-website-project/js/script.js
@@ -0,0 +1,42 @@
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+const input = document.getElementById('link-input');
+const linkForm = document.getElementById('link-form');
+const errMsg = document.getElementById('err_msg');
+
+linkForm.addEventListener('submit', formSubmit);
+btn.addEventListener('click', navToggle);
+
+function navToggle() {
+  btn.classList.toggle('open');
+  menu.classList.toggle('flex');
+  menu.classList.toggle('hidden');
+}
+
+function validURL(str) {
+  var pattern = new RegExp(
+    '^(https?:\\/\\/)?' + // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+      '((\\d{1,3}\\.){3}\\d{1,3}))' +
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+      '(\\?[;&a-z\\d%_.~+=-]*)?' +
+      '(\\#[-a-z\\d_]*)?$',
+    'i'
+  );
+  return !!pattern.test(str);
+}
+
+function formSubmit(e) {
+  e.preventDefault();
+  if (input.value == '') {
+    errMsg.innerHTML = `Please enter the link`;
+    input.classList.add('border-red');
+  } else if (!validURL(input.value)) {
+    errMsg.innerHTML = `Please enter a valid URL`;
+    input.classList.add('border-red');
+  } else {
+    errMsg.innerHTML = ` `;
+    input.classList.remove('border-red');
+    alert('Success');
+  }
+}
